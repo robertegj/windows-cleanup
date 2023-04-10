9afe7c7e48ecada260e1cf156f0803bcf7c430ac
Robert G <rob@anicyber.com>
2023-04-10 11:31:14 -0400

    Update cleanup_fresh_install.ps1

--- cleanup_fresh_install.ps1
+++ cleanup_fresh_install.ps1
@@ -1,6 +1,10 @@
+# Remove apps
 Get-AppXPackage | where-object {$_.name –notlike “*store*”} | Remove-AppxPackage
+# Remove Store
 Get-AppxPackage -Name "Microsoft.WindowsStore" | Remove-AppxPackage
+# Remove Todo app
 Get-AppxPackage -Name "Microsoft.todos" -AllUsers | Remove-AppxPackage -AllUsers
+# Remove pinned apps
 (New-Object -Com Shell.Application).
     NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').
     Items() |
